8e588e31546520e088e5b7bf5430dcfd0cf661a1
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2024-01-17 23:56:28 -0300

    create endpoint to upload

--- readme.md
+++ readme.md
@@ -2,3 +2,9 @@
 - [ ] Utilização do Cloudflare R2 para upload de arquivos
 - [ ] O upload deve ser feito diretamente pelo front-end utilizando Presigned URLs
 - [ ] Os links para compartilhamento devem ser assinados evitando acesso público
+
+### Exemplo de upload
+
+```js
+ http --form PUT https://example-upload-dev.10292b14168cb4e2d4f98c84ad0f699e.r2.cloudflarestorage.com/file.mp4\?X-Amz-Algorithm\=AWS4-HMAC-SHA256\&X-Amz-Content-Sha256\=UNSIGNED-PAYLOAD\&X-Amz-Credential\=a6d180224c7a6f3d0beeeaa1f097ff82%2F20240118%2Fauto%2Fs3%2Faws4_request\&X-Amz-Date\=20240118T025149Z\&X-Amz-Expires\=600\&X-Amz-Signature\=a1bf7b6ae8c32ff7115e967f710e5dcc78d6b0accf559a62cd50474716013f5f\&X-Amz-SignedHeaders\=host\&x-id\=PutObject "Content-Type":"video/mp4" < example.mp4 
+```
--- src/routes/v1/uploads.ts
+++ src/routes/v1/uploads.ts
@@ -3,6 +3,8 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { FastifyRequest, FastifyReply, FastifyInstance } from "fastify";
 import { env } from "@/env.ts";
 import { r2 } from "@/lib/cloudflare.ts";
+import { z } from "zod";
+import { prisma } from "@/db/prisma.ts";
 
 export const uploadsRoute = (
   server: FastifyInstance,
@@ -10,16 +12,33 @@ export const uploadsRoute = (
   done: () => void
   ) => {
   server.post('/uploads', async (request: FastifyRequest, reply: FastifyReply) => {
+    
+    const uploadBodySchema = z.object({
+      name: z.string().min(1),
+      contentType: z.string().regex(/^[\w\/+.-]+$/i),
+    })
+
+    const { name, contentType } = uploadBodySchema.parse(request.body)
+
+    const fileKey = `${name}-${Date.now()}`
 
     const signedURL = await getSignedUrl(
       r2,
       new PutObjectCommand({
         Bucket: env.CLOUDFLARE_BUCKET_NAME,
-        Key: 'my-key',
-        ContentType: 'text/plain',
+        Key: fileKey,
+        ContentType: contentType,
       }),
       { expiresIn: 600 }
     )
+
+    await prisma.file.create({
+      data: {
+        name,
+        key: fileKey,
+        contentType,
+      }
+    })
   
     reply.code(200).send(signedURL)
   });
